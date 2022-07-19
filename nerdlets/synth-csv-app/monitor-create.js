386daf384b3e3c367df307363baf6a753ad2c122
David Santia <davidsantia.morris@gmail.com>
2022-07-19 08:17:05 -0700

    Detect tags to update, create update map

--- nerdlets/synth-csv-app/monitor-create.js
+++ nerdlets/synth-csv-app/monitor-create.js
@@ -32,6 +32,7 @@ export default class MonitorCreate extends React.Component {
   }
 
   compareMonitor(obj, entity) {
+    var updateMap = {tags: {}};
     var updates = [];
     var tagMap = {};
     var status = 'Monitor exists, '
@@ -39,43 +40,72 @@ export default class MonitorCreate extends React.Component {
     for (const tag of entity.tags) {
       tagMap[tag.key] = tag.values;
     }
+    // check URL
     if (obj.uri !== entity.monitoredUrl) {
       updates.push('change URL: ' + obj.uri)
+      updateMap.uri = true;
     }
+    // check period
     if (tagMap.period?.length === 1) {
       if (obj.period !== this.freq2name[tagMap.period[0]]) {
         updates.push('change period: ' + obj.period)
+        updateMap.period = true;
       }
     }
+    // check locations
     if (tagMap.publicLocation?.length > 0) {
-      var adding = [];
-      var removing = [];
+      var addLocations = [];
+      var removeLocations = [];
       for (const label of obj.locationLabels) {
         if (!tagMap.publicLocation.includes(label)) {
-          adding.push(label)
+          addLocations.push(label)
         }
       }
       for (const label of tagMap.publicLocation) {
         if (!obj.locationLabels.includes(label)) {
-          removing.push(label)
+          removeLocations.push(label)
         }
       }
-      if (adding.length > 0) {
-        updates.push('add locations: ' + JSON.stringify(adding))
+      if (addLocations.length > 0) {
+        updates.push('add locations: ' + JSON.stringify(addLocations))
       }
-      if (removing.length > 0) {
-        updates.push('remove locations: ' + JSON.stringify(removing))
+      if (removeLocations.length > 0) {
+        updates.push('remove locations: ' + JSON.stringify(removeLocations))
       }
     }
+    // check tags
+    var addTags = [];
+    var removeTags = [];
+    for (const tag of obj.tags) {
+      if (tagMap[tag.key]) {
+        if (!tagMap[tag.key].includes(tag.values[0])) {
+          addTags.push(tag.key + ':' + tag.values[0]);
+          updateMap.tags[tag.key] = true;
+        }
+        for (const value of tagMap[tag.key]) {
+          if (value !== tag.values[0]) {
+            removeTags.push(tag.key + ':' + value);
+            updateMap.tags[tag.key] = true;
+          }
+        }
+      } else {
+        addTags.push(tag.key + ':' + tag.values[0]);
+        updateMap.tags[tag.key] = true;
+      }
+    }
+    if (addTags.length > 0) {
+      updates.push('add tags: ' + JSON.stringify(addTags))
+    }
+    if (removeTags.length > 0) {
+      updates.push('remove tags: ' + JSON.stringify(removeTags))
+    }
+
     if (updates.length === 0) {
       status += 'nothing to do';
     } else {
       status += 'Awaiting update to ' + updates.join(', ');
     }
-    console.log('Obj Tags:', JSON.stringify(obj.tags))
-    console.log('Entity Tags:', JSON.stringify(tagMap))
-
-    return status;
+    return [status, updateMap];
   }
 
   searchMonitor(obj) {
@@ -102,10 +132,12 @@ export default class MonitorCreate extends React.Component {
       })
       .then(entity => {
         var status;
+        var updateMap;
         // Get existing monitor attributes
         if (entity) {
-          status = this.compareMonitor(obj, entity);
+          [status, updateMap] = this.compareMonitor(obj, entity);
           //console.log('Monitor exists:', JSON.stringify(entity, null, 4))
+          console.log('To update:', JSON.stringify(updateMap));
         } else {
           status = 'Awaiting generation'
         }
